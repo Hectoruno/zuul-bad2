30e853792feafcb80db4a164f85303b679a931d3
Héctor García García <Hectoruno>
2019-05-29 11:05:59 +0200

    Commit 02. Nuestro proyecto ya tiene el nombre de las localizaciones.

--- Game.java
+++ Game.java
@@ -34,23 +34,25 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room hall, sala, comedor, habitaciones, patio, cuarto;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        hall = new Room("entrada del manicomio");
+        sala = new Room("sala de vigilancia");
+        comedor = new Room("en el comedor");
+        habitaciones = new Room("en las habitaciones");
+        patio = new Room("en el patio");
+        cuarto = new Room("en el cuarto de basuras");
         
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        hall.setExits(null, null, comedor, sala);
+        sala.setExits(null, hall, null, null);
+        comedor.setExits(hall, patio, null, habitaciones);
+        habitaciones.setExits(null, comedor, null, null);
+        patio.setExits(null, null, cuarto, comedor);
+        cuarto.setExits(patio, null, null, null);
 
-        currentRoom = outside;  // start game outside
+        currentRoom = hall;  // start game outside
     }
 
     /**
